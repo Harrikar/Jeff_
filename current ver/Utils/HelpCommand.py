f896073bdc09f538b683095afb7ce89fd74899e2
Harrikar <97301242+Harrikar@users.noreply.github.com>
2023-07-29 14:27:40 +0300

    updated help command

--- current ver/Utils/HelpCommand.py
+++ current ver/Utils/HelpCommand.py
@@ -1,11 +1,5 @@
 import disnake
 from disnake.ext import commands
-import AllianceCommand
-import LevelingSystem
-import NationCommand
-import ResCommand
-import ServerCommand
-import TownCommand
 from AllianceCommand import AllianceCommand
 from LevelingSystem import LevelingSystem
 from NationCommand import NationCommand
@@ -33,77 +27,13 @@ class HelpCog(commands.Cog):
 
 
 def register_commands(bot):
-     # Register all subcommands of the `nation` command
-    for subcommand in NationCommand.__cog_commands__:
-        if isinstance(subcommand, commands.Group):
-            for subcommand_option in subcommand.__cog_commands__:
-                bot.add_command(subcommand_option)
-        else:
-            bot.add_command(subcommand)
+    # Register commands from custom command classes
+    cogs = [AllianceCommand(), LevelingSystem(), NationCommand(), ResCommand(),
+            ServerCommand(), TownCommand(), VoteCommand(), WeatherCommand(), HelpCog(bot)]
 
+    for cog in cogs:
+        bot.add_cog(cog)
 
-    for subcommand in TownCommand.__cog_commands__:
-        if isinstance(subcommand, commands.Group):
-            for subcommand_option in subcommand.__cog_commands__:
-                bot.add_command(subcommand_option)
-        else:
-            bot.add_command(subcommand)
-
-    for subcommand in ResCommand.__cog_commands__:
-        if isinstance(subcommand, commands.Group):
-            for subcommand_option in subcommand.__cog_commands__:
-                bot.add_command(subcommand_option)
-        else:
-            bot.add_command(subcommand)
-
-
-    for subcommand in AllianceCommand.__cog_commands__:
-        if isinstance(subcommand, commands.Group):
-            for subcommand_option in subcommand.__cog_commands__:
-                bot.add_command(subcommand_option)
-        else:
-            bot.add_command(subcommand)
-
-
-
-    for subcommand in TownCommand.__cog_commands__:
-        if isinstance(subcommand, commands.Group):
-            for subcommand_option in subcommand.__cog_commands__:
-                bot.add_command(subcommand_option)
-        else:
-            bot.add_command(subcommand)
-
-
-    for subcommand in ServerCommand.__cog_commands__:
-        if isinstance(subcommand, commands.Group):
-            for subcommand_option in subcommand.__cog_commands__:
-                bot.add_command(subcommand_option)
-        else:
-            bot.add_command(subcommand)
-
-
-    for subcommand in VoteCommand.__cog_commands__:
-        if isinstance(subcommand, commands.Group):
-            for subcommand_option in subcommand.__cog_commands__:
-                bot.add_command(subcommand_option)
-        else:
-            bot.add_command(subcommand)
-
-
-    for subcommand in WeatherCommand.__cog_commands__:
-        if isinstance(subcommand, commands.Group):
-            for subcommand_option in subcommand.__cog_commands__:
-                bot.add_command(subcommand_option)
-        else:
-            bot.add_command(subcommand)
-
-
-    for subcommand in LevelingSystem.__cog_commands__:
-        if isinstance(subcommand, commands.Group):
-            for subcommand_option in subcommand.__cog_commands__:
-                bot.add_command(subcommand_option)
-        else:
-            bot.add_command(subcommand)
 
 def setup(bot):
-    bot.add_cog(HelpCog(bot))
+    register_commands(bot)
